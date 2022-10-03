47f5e0b8084cfb66660b1be09c4acd107d5e900a
emnlp2022draft <108164535+emnlp2022draft@users.noreply.github.com>
2022-10-02 21:18:16 -0400

    Update 004_extract_attention.py

--- 004_extract_attention.py
+++ 004_extract_attention.py
@@ -139,7 +139,7 @@ def main(process_position):
 
 if __name__ == "__main__":
 
-    dataset = 'SemEval2017'
+    dataset = 'Nguyen2007'
     text_path = dataset + '/processed_docsutf8/'
     output_path = dataset + '/processed_' + dataset + '/'
     save_path = output_path + 'sentence_paired_text/'

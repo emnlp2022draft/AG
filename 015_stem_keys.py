a8df1ef6540fa1cf003fe16e43834865049ad901
emnlp2022draft <108164535+emnlp2022draft@users.noreply.github.com>
2022-10-02 21:31:19 -0400

    Update 015_stem_keys.py

--- 015_stem_keys.py
+++ 015_stem_keys.py
@@ -20,7 +20,7 @@ for c, file in enumerate(files[:]):
     # print('file:', file)
 
     document_id = file.replace('.txt', '')
-    text = read_text(doc_path+file)
+    # text = read_text(doc_path+file)
 
     keys = read_text(key_path+file.replace('txt', 'key'))
     keys = keys.split('\n')
@@ -35,6 +35,6 @@ for c, file in enumerate(files[:]):
             stemmed_keys.append(stemmed_key)
     # print('stemmed_keys:', stemmed_keys)
 
-    w.writerow([file] + stemmed_keys)
+    w.writerow([document_id] + stemmed_keys)
 
 
